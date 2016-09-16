34a2e99385263469fd9d00ff7ecf3a8b52e17283
pgd <progamedesigns@gmail.com>
2016-09-16 16:45:56 -0700

    Initial Commit

---
+++ GUIGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GUIGame
+//
+//  Created by max dietz on 9/16/16.
+//  Copyright © 2016 PGD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
