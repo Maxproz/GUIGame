960fbc4a79e3e392caaab3230370e314fc175974
pgd <progamedesigns@gmail.com>
2016-09-17 13:23:06 -0700

    Added code to create a square.

--- GUIGame/main.cpp
+++ GUIGame/main.cpp
@@ -172,7 +172,48 @@ glBindVertexArray(VAO);
     glEnableVertexAttribArray(0);
 // Unbind the Vertex Array Object (VAO);
 glBindVertexArray(0);
+
     
+
+    // Draw Square Code
+    GLfloat verticesSquare[] =
+    {
+        0.5f,  0.5f, 0.0f, // top right
+        0.5f, -0.5f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f // bottom left
+       -0.5f,  0.5f, 0.0f  // top left
+    };
+    
+    GLuint indicesSquare[] =
+    {
+        0, 1, 3, // we start from 0
+        1, 2, 3
+    };
+    
+    glGenVertexArrays(1, &VAO);
+    // Create element buffer object for our square
+    GLuint EBO;
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesSquare), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesSquare), indicesSquare, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+glBindVertexArray(0);
+    
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     
     // Program loop
     while(!glfwWindowShouldClose(window))
@@ -190,16 +231,16 @@ glBindVertexArray(0);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         
         // Swap the buffers
         glfwSwapBuffers(window);
-        
-        
     }
-    
-
-    
+    // properly deallocate all resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     
     glfwTerminate();
     return 0;
