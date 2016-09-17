8e15f729f9e2cc173d3097210120e9ea6a870479
pgd <progamedesigns@gmail.com>
2016-09-16 19:04:39 -0700

    The window is created and the blinking problem was fixed by adding color and GLClear

--- GUIGame/main.cpp
+++ GUIGame/main.cpp
@@ -8,8 +8,76 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <GL/glew.h>
+#include <glfw3.h>
+
+#ifdef __APPLE__
+#  include <GLUT/glut.h>
+#else
+#  include <GL/glut.h>
+#endif
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action , int mode);
+
+int main(int argc, const char* argv[])
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+//    GLFWwindow
+    GLFWwindow* window = glfwCreateWindow(800, 600, "BlackJack", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initalize GLEW" << std::endl;
+        return -1;
+    }
+    
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    
+    glViewport(0, 0, width, height);
+    
+    glfwSetKeyCallback(window, key_callback);
+    
+    // Program loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // Check and call events.
+        glfwPollEvents();
+        
+        // Rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Swap the buffers
+        glfwSwapBuffers(window);
+        
+        
+    }
+    
+    glfwTerminate();
     return 0;
 }
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // When a user presses the space key, we set the window should close property to true,
+    // closing the application
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
